1d27b5da5503dc6fd441ffb177ff04be443ab7bc
Satoru <you@example.com>
2023-02-22 07:16:41 +0900

    fix: bug

--- src/Service/Form/ConfigFormFactory.php
+++ src/Service/Form/ConfigFormFactory.php
@@ -13,7 +13,7 @@ class ConfigFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $form = new ConfigForm(null, $options);
+        $form = new ConfigForm(null, $options ?? []);
         $form
             ->setTranslator($services->get('MvcTranslator'))
             ->setEventManager($services->get('EventManager'));
